48512d519b2159cbbcfbed9261d21a3f804d1296
darmawanaldin12 <darmawanaldin12@gmail.com>
2026-05-28 10:40:17 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,14 +1,5 @@
 import { redirect } from 'next/navigation'
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
 
-export default async function Home() {
-  const supabase = createServerComponentClient({ cookies })
-  const { data: { session } } = await supabase.auth.getSession()
-
-  if (!session) {
-    redirect('/login')
-  } else {
-    redirect('/dashboard')
-  }
+export default function Home() {
+  redirect('/dashboard')
 }
