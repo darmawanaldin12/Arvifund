e314ad404f1e2a792ee41582b41687f3fb63739a
darmawanaldin12 <darmawanaldin12@gmail.com>
2026-06-03 07:25:36 +0700

    deploy: update components/ui/progress.jsx

--- components/ui/progress.jsx
+++ components/ui/progress.jsx
@@ -1,17 +1,20 @@
-'use client'
-import * as React from 'react'
-import * as ProgressPrimitive from '@radix-ui/react-progress'
-import { cn } from '@/lib/utils'
+"use client"
+import * as React from "react"
+import * as ProgressPrimitive from "@radix-ui/react-progress"
+import { cn } from "../../lib/utils-cn"
 
-const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
+const Progress = React.forwardRef(({ className, value, indicatorClassName, style, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
-    className={cn('relative h-2 w-full overflow-hidden rounded-full bg-primary/20', className)}
+    className={cn(
+      "relative h-2 w-full overflow-hidden rounded-full bg-[var(--surface2)]",
+      className
+    )}
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      className={cn("h-full w-full flex-1 transition-all duration-500", indicatorClassName || "bg-[var(--accent)]")}
+      style={{ transform: `translateX(-${100 - (value || 0)}%)`, ...style }}
     />
   </ProgressPrimitive.Root>
 ))
