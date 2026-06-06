b202a18537131d7cdcbf18989a28a3ed3d91ae08
darmawanaldin12 <darmawanaldin12@gmail.com>
2026-06-07 03:11:13 +0700

    fix: icon.js read logo.png from local fs instead of fetching URL

--- app/icon.js
+++ app/icon.js
@@ -1,9 +1,15 @@
 import { ImageResponse } from 'next/og'
+import { readFile } from 'fs/promises'
+import { join } from 'path'
 
 export const size = { width: 512, height: 512 }
 export const contentType = 'image/png'
 
-export default function Icon() {
+export default async function Icon() {
+  // Baca logo.png dari public folder secara lokal
+  const logoData = await readFile(join(process.cwd(), 'public', 'logo.png'))
+  const logoBase64 = `data:image/png;base64,${logoData.toString('base64')}`
+
   return new ImageResponse(
     (
       <div
@@ -19,7 +25,7 @@ export default function Icon() {
       >
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          src="https://arvifund.vercel.app/logo.png"
+          src={logoBase64}
           width={400}
           height={347}
           alt="Arvifund"
